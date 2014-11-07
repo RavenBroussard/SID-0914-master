c76aa76697fb041b09d7e1b1b6e1cc9dcaede16d
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-11-06 22:48:38 -0600

    rename file

---
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a(2)/Broussard_Raven_Project2.js
@@ -0,0 +1,73 @@
+/* Raven "Nikki" Broussard
+	SID 1114
+	Project 2
+*/
+
+var customerWantsPhone = true;
+var customerName = "Jim";
+var salesperson = "Nikki"
+var net10 = "Unlimited Service";
+var h20 = "Limited Services;
+var rhino = true
+var planPrices = [30, 40, 50, 60];
+
+// Procedure
+say ("Welcome to Wilson's Electronics, would you to try new phone service today?");
+	var greetCustomer = function(newService){
+	if (newService === true){
+	say("We have several service providers to choose from, and you can keep your old phone.");
+	} else {
+	say("We can lower your bill with no monthly contract.");
+	}
+	};
+	
+// Boolean Function
+
+var customerPhone = function (customerPhone, serviceType) {
+	var sellService = true;
+	if (customerPhone >=1)
+		say("Awesome, let me tell you about the cool plans we have.");
+	} else {
+		say("Cool, let's discuss some family plans.");
+	}
+		if (
+		say("O.K. let me show you some phones
+		
+		
+//Number Function
+
+var saveMoney = function(amountSaved){
+	while (amountSaved is > 400){
+		say("I can save you at least $" + () + "or more a year.");
+		amountSaved+=20;
+	}
+	say("After doing the math I can actually save you $600!");
+	
+return amountSaved;
+};
+
+//StringFunction
+
+var needPhone = function(firstPhone, secondPhone){
+	var purchasePhone = ["Yes", "No"];
+	var priceRange = ["great", "a little to much today"];
+		say("I have the perfect phone for you" + firstPhone + " and the " + secondPhone
++ "in stock today.");
+		say("The " + firstPhone + " price " + priceRange[0] + " but the " + secondPhone + " is " + priceRange[2].);
+	var finalDecision = (purchaseItems[0] + ", I am going to get" + firstPhone + ", but" + purchaseItems[1] + " I'm going to have to pass on the " + secondPhone + ", thanks for the help.");
+return finalDecision;
+};
+
+//Array Function
+var cellService = function (planPrice, bestPrice){
+	for (var i = 0; i < planPrice.length; i++){
+	var lowerplan = planPrice[i];
+		say("The best deal we have at the moment is  " + bestPrice + " plus activation cost. Then the next best deal is " + lowerplan + " plus activation cost.");
+		}
+return lowerplan;
+};
+
+Returned Values
+
+ 
+	
