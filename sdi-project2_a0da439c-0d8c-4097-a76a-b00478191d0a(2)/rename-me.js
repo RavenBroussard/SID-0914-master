d37dbf2ac54b246a91d05c8ba0fe21d950890614
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-11-06 20:37:33 -0600

    declaring variables

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a(2)/rename-me.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a(2)/rename-me.js
@@ -1,1 +1,12 @@
-alert("JavaScript works!");
+/* Raven "Nikki" Broussard
+	SID 1114
+	Project 2
+*/
+
+var customerWantsPhone = true;
+var customerName = "Jim";
+var salesperson = "Nikki"
+var net10 = "Unlimited Service";
+var h20 = "Limited Services;
+var rhino = true
+
