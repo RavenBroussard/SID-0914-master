8685826a896a33a8da875df7a82fe184666f9c85
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-11-06 20:55:02 -0600

    Procedure
    added procedure to the code

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a(2)/rename-me.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a(2)/rename-me.js
@@ -10,3 +10,16 @@ var net10 = "Unlimited Service";
 var h20 = "Limited Services;
 var rhino = true
 
+// Procedure
+say ("Welcome to Wilson's Electronics, would you to try new phone service today?");
+	var greetCustomer = function(newService){
+	if (newService === true){
+	say("We have several service providers to choose from, and you can keep your old phone.");
+	} else {
+	say("We can lower your bill with no monthly contract.");
+	}
+	};
+	
+// Boolean Function
+
+
