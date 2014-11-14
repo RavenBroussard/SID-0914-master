374de748388f5c740ea794189694c86dac745b01
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-11-13 21:46:31 -0600

    Objects

--- sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Broussard_Raven_assignment3.js
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Broussard_Raven_assignment3.js
@@ -11,3 +11,49 @@ var moneyPriority = 2;
 var knownProblems = ["no vacancies", "long lines", "not enough time"];
 var packedEverything = true;
 var luggageLost = false;
+
+// Objects
+var theAirlines = [
+		Delta = {overbooked: false, pilotName: "Larry", discountDays; ["Tuesday", "Wednesday", "Thursday"]}
+		Southwest = {, pilotName: "James", discountDays; ["Monday", "Sunday" "Friday"]}];
+		
+var  = function (item) {
+		var gateNumber = "";
+		var setGate = function (num){  //sets gate number we will leave from
+				gateNumber = num;
+				return gateNumber;
+		}
+		
+		var departure = "";
+		var setDeparture = function (newDeparture) { //sets up/down
+			departure = newDeparture;
+			return departure;
+		} 
+		
+		var hours = 8;
+		var addHours = function (addHours){   //adds time to the vacation
+			return (hours + addHours);
+		}
+		
+		var getFlight = function (){   //return day or night flight
+			return "daytime";
+		}
+		
+		var vacationItinerary = funtion (extreme) {  
+			for (var i = 0; i < extreme.vactionSpots.length; i++){
+			var vacy = extreme.vacationSpots[i];
+			console.log("Gate Number: " + vacy.gatenumber + " is where we have to depart: " + vacy.getflight + "we have" + vacy.hours + " and our flight is currently "  + vacy.status + ".");
+		}
+	}
+	
+	return {
+			"addHours":       addHours,
+			"setDeparture":   setDeparture,
+			"getFlight":    getFlight,
+			"vacationItinerary": vacationItinerary,
+			"setGate":  setGate,
+			
+	}
+	
+	
+	
