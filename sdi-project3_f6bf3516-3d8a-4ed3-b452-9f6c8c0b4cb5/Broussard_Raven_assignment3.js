08aeeac87ccae6931df6e831382586d77ca73710
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-11-13 19:49:42 -0600

    variables

---
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Broussard_Raven_assignment3.js
@@ -0,0 +1,13 @@
+/*  SDI Project 3
+	Raven Broussard
+	SDI November 13, 2014
+*/
+
+// Variables
+var familyMembers = ["Raven", "Cayla", "Cyrus"];
+var vacationSpots = ["Statue of Liberty", "Lego Land", "Disney World"];
+var moneyAvailable = true;
+var moneyPriority = 2;
+var knownProblems = ["no vacancies", "long lines", "not enough time"];
+var packedEverything = true;
+var luggageLost = false;
